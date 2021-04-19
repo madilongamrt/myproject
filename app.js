78d34caa4a5de0b365580ad7e70c987eaa083e54
rudzani Matshotshi <madilongamrt@gmail.com>
2021-04-19 12:07:32 +0200

    docker

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
-var express = require('express');
-var app = express();
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+const express = require("express");
+const http = require("http");
+
+const app = express();
+
+const server = http.createServer(app);
+
+app.get('/', function(req, res) {
+    res.send("Hello World");
 });
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+
+server.listen(3000, function() {
+    console.log("Server is listening on port: 3000");
 });
